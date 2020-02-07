439c97bec730f3b06717db518c4261a180a236ab
raushan2016 <raushankumar9666@gmail.com>
2020-02-07 02:21:09 +0000

    fix the script

--- ETCD/scripts/restore.sh
+++ ETCD/scripts/restore.sh
@@ -7,10 +7,10 @@ echo "Starting snapshot restore"
 etcdctl --endpoints=https://127.0.0.1:2379 --cacert=${CERT_PATH}/ca.crt --cert=${CERT_PATH}/etcdclient.crt --key=${CERT_PATH}/etcdclient.key snapshot restore ${SNAPSHOT_PATH}
 
 echo "Restore permission to the db"
-chown -R etcd:etcd /default.etcd
+chown -R etcd:etcd default.etcd
 
 echo "Copy db files from snapshot to etcd datadir"
-cp -rp /default.etcd/member/ ${ETCD_DATADIR}
+cp -rp default.etcd/member/ ${ETCD_DATADIR}
 
 echo "Restart master node"
 reboot
