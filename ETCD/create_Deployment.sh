9536a5e2ba574e974fb549198a3074463a118146
raushan2016 <raushankumar9666@gmail.com>
2020-02-07 02:32:03 +0000

    fixed restore

--- ETCD/create_Deployment.sh
+++ ETCD/create_Deployment.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-for i in {1..100}
+for i in {1..2}
     do
       echo "creating deployment hello-kubernetes$i "
       name=hello-kubernetes$i
-      sed "s/hello-kubernetes1/${name}/g" ./nginx-app.yaml > nginx-app-temp.yaml
-      kubectl apply -f ./nginx-app-temp.yaml -n foo1
+      sed "s/hello-kubernetes/${name}/g" ./nginx-app.yaml > nginx-app-temp.yaml
+      kubectl apply -f ./nginx-app-temp.yaml
 
     done
